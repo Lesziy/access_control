ed33f90913453e9d23dc39a11322758cc918f434
lesziy <przemyslawsaramonowicz@o2.pl>
2016-05-12 22:27:07 +0200

    dodany kawalek kalendarza

--- djApplication/views.py
+++ djApplication/views.py
@@ -1,9 +1,12 @@
-
+#-*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-
+from calendar import Calendar
+from datetime import date
+months = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Pazdziernik', 'Listopad', 'Grudzień']
+dayNames = ['Pon', 'Wt', 'Śr', 'Czw','Pt','Sob','Nie']
 def index(request):
 	# 'request' contains info about user
 	#context = RequestContext(request)
@@ -15,7 +18,7 @@ def index(request):
 	#hold session
 	if "username" in request.session:
 		print (request.session["username"] + " is logged in")
-		return render(request, 'index.html')
+		return calendarSite(request, date.today().year, date.today().month-1)
 	#basic login template 
 	return render(request, 'reservationSite.html')
 	
@@ -24,8 +27,15 @@ def index(request):
 def loginSite(request):
 	if(login(request)):
 		request.session["username"] = request.POST['user']
-		return render(request, 'index.html')
-	
+		return calendarSite(request, date.today().year, date.today().month-1)
+def calendarSite(request, year, month):
+   
+   cal = Calendar()
+
+   weeks = cal.monthdayscalendar(year = year, month = month)
+   
+   return render(request,'calendar.html',{'year': year, 'month': months[month] ,'dayNames' : dayNames, 'weeks' : weeks})
+
 def logout(request):
 	del request.session["username"]
 	return
