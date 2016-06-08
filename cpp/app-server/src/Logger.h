1a7c538b1232fa05f6d35e26c1e50c4bf924e035
Galfaroth7 <misiekostap@wp.pl>
2016-06-08 23:37:58 +0200

    Added logger method

--- cpp/app-server/src/Logger.h
+++ cpp/app-server/src/Logger.h
@@ -1,9 +1,10 @@
+#pragma once
 /*
- * Logger.h
- *
- *  Created on: 7 maj 2016
- *      Author: secman
- */
+* Logger.h
+*
+*  Created on: 7 maj 2016
+*      Author: secman
+*/
 
 #ifndef LOGGER_H_
 #define LOGGER_H_
@@ -11,14 +12,18 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <time.h>
 
 class Logger {
 public:
-    Logger(std::string logFilePath);
-    ~Logger();
+	Logger(std::string logFilePath);
+	~Logger();
+	void log(std::string ip, std::string user, std::string type, std::string description);
+	std::string getCurrentTime();
 
 private:
-    std::ofstream logStream;
+	std::ofstream logStream;
 };
 
 #endif /* LOGGER_H_ */
+
